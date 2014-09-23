83668319927f519568fcb26f17fb0f465bda0423
ninjinx <thomastolbol@gmail.com>
2014-09-23 16:53:53 +0200

    Simple layers can now be added

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-function Layer(id){
-    this.id = id;
-    this.type;
-    this.alignment = 0;
-    this.x = 160;
-    this.y = 160;
-    this.r = 0;
+layers = [];
+var canvas, ctx;
+var fps = 60;
+var fonts = ["Roboto", "lol"]; 
+var activeLayer;
+
+$(document).ready(function() {
+    //Set up listeners
+    $("#newlayerbutton").click(function() {
+        newLayer();
+    });
+    
+    canvas = document.getElementById('watchcanvas');
+    ctx = canvas.getContext('2d');
+    
+    loop();
+});
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for(var i = 0; i < layers.length; i++){
+        layers[i].draw(ctx);
+    }
+    
+    requestAnimFrame(function() {
+        loop();
+    });
 }
 
-layers = [];
+function newLayer() {
+    $("#layerlist li").removeClass("active");
+    $("#layerlist").append("<li class='active'>Text" + layers.length + "</li>");
+    layers.push(new Layer(layers.length, "text"));
+    activeLayer = layers[layers.length-1];
+    updateLayerListeners();
+}
+
+function updateLayerListeners() {
+    $("#layerlist li").click(function() {
+        $("#layerlist li").removeClass("active");
+        $(this).addClass("active");
+    });
+}
+
+window.requestAnimFrame = (function() {
+    return  window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000.0 / 60);
+            };
+})();
