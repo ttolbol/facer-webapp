8f83e8756d188badf5448ee66001e2dec50ddbc7
Nick <natureman1.verizon@gmail.com>
2014-09-28 13:06:17 -0400

    Forked
    Here are some things that I did yesterday, before "Material Design", so
    merging back in will take some time.
    -Defined constants
    -Fixed some UI
    -LayerManager greatly improves the layer experience by adding, finding,
    and removing layers and mirroring them in the UI
    -Other things

--- js/layer.js
+++ js/layer.js
@@ -1,7 +1,43 @@
-function Layer(id, type) {
-    this.id = id;
+//Here are some constants for easier setting
+//ALIGNMENTS
+var LEFT_ALIGN = 0;
+var CENTER_ALIGN = 1;
+var RIGHT_ALIGN = 2;
+var ALIGNMENTS = [LEFT_ALIGN, CENTER_ALIGN, RIGHT_ALIGN];
+
+//FONTS
+var ROBOTO_THIN = 0;
+var ROBOTO_LIGHT = 1;
+var ROBOTO_CONDENSED_LIGHT = 2;
+var ROBOTO = 3;
+var ROBOTO_BLACK = 4;
+var ROBOTO_CONDENSED = 5;
+var ROBOTO_SLAB_THIN = 6;
+var ROBOTO_SLAB_LIGHT = 7;
+var ROBOTO_SLAB = 8;
+var CUSTOM_FONT = 9;
+var FONTS = [ROBOTO_THIN, ROBOTO_LIGHT, ROBOTO_CONDENSED_LIGHT, ROBOTO, ROBOTO_BLACK, ROBOTO_CONDENSED, ROBOTO_SLAB_THIN, ROBOTO_SLAB_LIGHT, ROBOTO_SLAB, CUSTOM_FONT];
+
+//LAYER TYPES
+var TEXT = "text";
+var SHAPE = "shape";
+var IMAGE = "image";
+var LAYER_TYPES = [new LayerType(TEXT), new LayerType(SHAPE), new LayerType(IMAGE)];
+
+//TEXT TRANSFORMS
+var DEFAULT_TEXT = 0;
+var UPPERCASE = 1;
+var LOWERCASE = 2;
+var TEXT_TRANSFORMS = [DEFAULT_TEXT, UPPERCASE, LOWERCASE];
+
+
+//The data parameter can be a JSON or some other data form which will allow layers to be loaded with data already
+function Layer(id, type, data) {
+    if(data === undefined)
+        data = {};
     this.type = type;
-    this.alignment = 0;
+    this.id = id || this.type.substring(0,1).toUpperCase()+this.type.substring(1) + layerManager.length;
+    this.alignment = LEFT_ALIGN;
     this.x = 160;
     this.y = 160;
     this.r = 0;
@@ -9,29 +45,37 @@ function Layer(id, type) {
     this.low_power = true;
     this.opacity = 100;
     this.size = 24;
+    this.index = 0;
 
     if (type === "text") {
-        this.text = "Text" + id;
+        this.text = data.text || this.id;
         this.color = "#FFF";
         this.bgcolor = "#000";
         this.low_power_color = "#000";
-        this.font_family = 0;
+        this.font_family = ROBOTO_THIN;
         this.bold = false;
         this.italic = true;
-        this.transform = 0;
+        this.transform = DEFAULT_TEXT;
     }
-
-
 }
 
+//This will be the actual function to output to the layer list -- thus the OO model will be easier to update
+//TODO Create a layerManager Class 
+Layer.prototype.toLayerHTML = function() {
+    var out= "<li layer='" + layerManager.length + "' class='active'><span class='primaryIcons'>&nbsp;<span class=\"glyphicon glyphicon-eye-open hideviewlayer\"></span><span class=\"glyphicon glyphicon-" + new LayerType(this.type).icon + "\"></span></span> <span class='layerName'>" + this.id;
+    if(this.type == TEXT)
+        out += "&nbsp;<small>("+this.text+")</small>";
+    out += "</span><div class=\"options\"><span class=\"glyphicon glyphicon-tag\"></span><span class=\"glyphicon glyphicon-resize-vertical\"></span></div></li>";
+    return out;
+}
 Layer.prototype.draw = function(ctx) {
     if (this.type === "text") {
-        if (this.alignment === 0) {
+        if (this.alignment === LEFT_ALIGN) {
             ctx.textAlign = "left";
-        } else if (this.alignment === 1) {
+        } else if (this.alignment === CENTER_ALIGN) {
             ctx.textAlign = "center";
         }
-        else if (this.alignment === 2) {
+        else if (this.alignment === RIGHT_ALIGN) {
             ctx.textAlign = "right";
         }
 
@@ -40,3 +84,57 @@ Layer.prototype.draw = function(ctx) {
         ctx.fillText(this.text, this.x, this.y);
     }
 };
+//If there are ever more types of things, this class will make it really easy to add another
+function LayerType(name) {
+    this.name = name;
+    switch(name) {
+        case IMAGE:
+            this.icon = "picture";
+            break;
+        case SHAPE:
+            this.icon = "heart";
+            break;
+        default: 
+            this.icon = "font";
+    }
+}
+function LayerManager() {
+    this.activeLayer;
+    this.length = 0;
+    this.layers = [];
+    LayerManager.prototype.add = function(nL) {
+        if(nL === undefined)
+            nL = new Layer(undefined, "text");
+        nL.index = this.layers.length;
+        $("#layerlist li").removeClass("active");
+        $("#layerlist").append(nL.toLayerHTML());    
+        this.layers.push(nL);
+        this.activeLayer = this.layers[this.layers.length - 1];
+        updateLayerListeners();
+        updateParams();
+        this.length = this.layers.length;
+    };
+    //Can find a layer from it's id, or returns false if not
+    LayerManager.prototype.find = function(id, case_sensitive) {
+        if(case_sensitive === undefined)
+            case_sensitive = true;
+        for(i in this.layers) {
+            if(this.layers[i].id == id && case_sensitive) {
+                return this.layers[i];   
+            } else if(this.layers[i].id.toLowerCase() == id.toLowerCase() && !case_sensitive)
+                return this.layers[i];
+        }
+        return false;
+    };
+    //Finds and returns index: can be used for deletion
+    LayerManager.prototype.indexOf = function(id, case_sensitive) {
+        if(this.find(id, case_sensitive) !== false)
+            return this.find(id, case_sensitive).index;
+        return false;
+    };  
+    LayerManager.prototype.delete = function(index) {
+        $('li[layer='+index+']').remove();
+        return this.layers.slice(index,index+1);
+    }
+}
+layerManager = new LayerManager();
